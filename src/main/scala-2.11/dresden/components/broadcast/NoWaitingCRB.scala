cba54d4e403a5755da18b6fc0df9b7f83601ad6f
Johan Mickos <jmickos@bu.edu>
2017-04-23 12:45:25 +0200

    Implement no-waiting causal reliable broadcast

---
+++ src/main/scala-2.11/dresden/components/broadcast/NoWaitingCRB.scala
@@ -0,0 +1,59 @@
+package dresden.components.broadcast
+
+import com.typesafe.scalalogging.StrictLogging
+import dresden.components.Ports._
+import dresden.components.broadcast.NoWaitingCRB.DataMessage
+import se.sics.kompics.KompicsEvent
+import se.sics.kompics.sl._
+import se.sics.ktoolbox.util.network.KAddress
+
+import scala.collection.mutable.ListBuffer
+
+// TODO Data type?
+object NoWaitingCRB {
+
+    case class DataMessage(past: List[Any], payload: KompicsEvent) extends KompicsEvent
+
+}
+
+class NoWaitingCRB(init: Init[NoWaitingCRB]) extends ComponentDefinition with StrictLogging {
+    val crb = provides[CausalOrderReliableBroadcast]
+
+    val rb = requires[ReliableBroadcast]
+
+    val (self) = init match {
+        case Init(s: KAddress) => s
+    }
+
+    private var delivered = Set.empty[KompicsEvent]
+    // TODO Data type?
+    private var past = ListBuffer.empty[Any] // TODO Data type?
+
+    crb uponEvent {
+        case CRB_Broadcast(payload) => handle {
+            trigger(RB_Broadcast(NoWaitingCRB.DataMessage(past.toList, payload)) -> rb)
+            past += (self, payload)
+        }
+    }
+
+    rb uponEvent {
+        case RB_Deliver(src, payload@NoWaitingCRB.DataMessage(mpast, msg)) => handle {
+            if (!delivered.contains(payload)) {
+                for ((s: KAddress, n: KompicsEvent) <- mpast) {
+                    if (!delivered.contains(n)) {
+                        trigger(CRB_Deliver(s, n) -> crb)
+                        delivered += n
+                        if (!past.contains((s, n))) {
+                            past += (s, n)
+                        }
+                    }
+                }
+                trigger(CRB_Deliver(src, msg) -> crb)
+                delivered += msg
+                if (!past.contains((src, msg))) {
+                    past += (src, msg)
+                }
+            }
+        }
+    }
+}
--- src/main/scala-2.11/dresden/components/broadcast/WaitingCRB.scala
+++ src/main/scala-2.11/dresden/components/broadcast/WaitingCRB.scala
@@ -8,7 +8,11 @@ import se.sics.ktoolbox.util.network.KAddress
 
 import scala.collection.mutable.ListBuffer
 
-case class DataMessage(timestamp: VectorClock, payload: KompicsEvent) extends KompicsEvent
+object WaitingCRB {
+
+    case class DataMessage(timestamp: VectorClock, payload: KompicsEvent) extends KompicsEvent
+
+}
 
 class WaitingCRB(init: Init[WaitingCRB]) extends ComponentDefinition {
 
@@ -19,7 +23,7 @@ class WaitingCRB(init: Init[WaitingCRB]) extends ComponentDefinition {
         case Init(s: KAddress, t: Set[KAddress]@unchecked) => (s, VectorClock.empty(t.toSeq))
     }
 
-    var pending: ListBuffer[(KAddress, DataMessage)] = ListBuffer()
+    var pending: ListBuffer[(KAddress, WaitingCRB.DataMessage)] = ListBuffer()
     var lsn = 0
 
 
@@ -28,12 +32,12 @@ class WaitingCRB(init: Init[WaitingCRB]) extends ComponentDefinition {
             val w = VectorClock.apply(vec)
             w.set(self, lsn)
             lsn = lsn + 1
-            trigger(RB_Broadcast(DataMessage(w, x.payload)) -> rb)
+            trigger(RB_Broadcast(WaitingCRB.DataMessage(w, x.payload)) -> rb)
         }
     }
 
     rb uponEvent {
-        case RB_Deliver(src: KAddress, msg: DataMessage) => handle {
+        case RB_Deliver(src: KAddress, msg: WaitingCRB.DataMessage) => handle {
             pending += ((src, msg))
             for (it@(address, message) <- pending.sortWith(_._2.timestamp <= _._2.timestamp)) {
                 if (message.timestamp <= vec) {
